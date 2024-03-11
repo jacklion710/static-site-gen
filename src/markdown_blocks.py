cb42576c8f27c7cd95988c67fe628797808b8f02
jacklion710 <jack.lion710@gmail.com>
2024-03-11 01:03:07 -0400

    Removed debugging code

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,11 +1,3 @@
-md = """# This is a heading
-
-This is a paragraph of text. It has some **bold** and *italic* words inside of it.
-
-* This is a list item
-* This is another list item
-"""
-
 def markdown_to_blocks(markdown):
     blocks = []
     lines = markdown.split("\n")
@@ -21,7 +13,3 @@ def markdown_to_blocks(markdown):
         blocks.append("\n".join(temp).strip())
 
     return blocks
-        
-
-
-print(markdown_to_blocks(md))
